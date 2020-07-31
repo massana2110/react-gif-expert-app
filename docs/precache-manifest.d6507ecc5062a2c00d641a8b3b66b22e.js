901b72989b5e0e45b4d430ad52c5d65702de42e5
David Massana <00072416@uca.edu.sv>
2020-07-30 20:09:35 -0600

    docs directory created

---
+++ docs/precache-manifest.d6507ecc5062a2c00d641a8b3b66b22e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7bec2068b4e274dbf579873b076b544",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcec97ddbd16eaff96a1",
+    "url": "/static/css/main.a60560b0.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcec97ddbd16eaff96a1",
+    "url": "/static/js/main.031e496e.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
